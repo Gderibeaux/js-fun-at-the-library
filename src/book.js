961be95752ec711a108a611975e8f60fbb553187
Gus Deribeaux <108699203+Gderibeaux@users.noreply.github.com>
2022-08-25 22:02:46 -0400

    Update book.js

--- src/book.js
+++ src/book.js
@@ -1,12 +1,49 @@
-function createTitle() {
-  
+function createTitle(title1) {
+  return (`The ${title1}`)
 }
+function buildMainCharacter(name, age, pronouns){
+var character = {
+  name: name,
+  age: age,
+  pronouns: pronouns,
+}
+return character
+}
+
+function saveReview(firstReview, reviews) {
+
+if (reviews.includes(firstReview) === false) {
+  return reviews.push(firstReview);
+}
+}
+
+function calculatePageCount (bookTitle, bookPageCount){
+  var bookPageCount = (bookTitle.length * 20)
+  return bookPageCount;
+}
+ function writeBook (bookTitle, bookCharacter, genre){
+   var bookWriter = {
+ title: bookTitle,
+ mainCharacter: bookCharacter,
+ pageCount: bookTitle.length * 20,
+ genre: genre,
+}
+return bookWriter
+ }
+
+ function editBook (book){
+   return book.pageCount = book.pageCount * .75 
+
+ }
+
+
+
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
